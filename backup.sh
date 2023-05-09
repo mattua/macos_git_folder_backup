cbba89a201cf74315debdb3f873cdb6f349331d5
Matt Thomas <matt.thomas@ihsmarkit.com>
2023-05-09 13:56:33 +0100

    sh script

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+brew install git
+read -p 'frequency in minutes (enter 0 to cancel and delete backups): ' freq
+PWD2=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+dir=$PWD2/.git_backup_scripts
+mkdir -p "$dir"
+FILE=$dir/backup.sh
+echo $FILE
+touch "$FILE"
+chmod +x "$FILE"
+cat > "$FILE" <<- EOM
+cd "$PWD2"
+git init
+git add .
+git commit -m "auto backup"
+EOM
+
+if [[ $freq -gt 0 ]]
+then
+(crontab -l|grep -v "$PWD2"; echo "*/$freq * * * * \"$FILE\" >> \"$dir/script.log\" 2>&1")|awk '!x[$0]++'|crontab -
+else
+ rm -rf $dir
+ rm -rf $PWD2/.git
+(crontab -l|grep -v "$PWD2";)|awk '!x[$0]++'|crontab -
+fi
